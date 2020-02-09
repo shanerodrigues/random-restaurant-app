06ca22fb67124fbd30cc1cf48c3e3365b1b8f3f6
shanerodrigues <shane2010r@hotmail.com>
2020-02-09 17:01:32 +1100

    Updates

---
+++ precache-manifest.3b92ed050cecd321492b583befef6eb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4093000e059fd3de2ac077a92c56a21d",
+    "url": "/random-restaurant-app/index.html"
+  },
+  {
+    "revision": "0a157136346f07162a88",
+    "url": "/random-restaurant-app/static/css/main.61bdb940.chunk.css"
+  },
+  {
+    "revision": "ef85fd727cbf5452a076",
+    "url": "/random-restaurant-app/static/js/2.fb4a4f0c.chunk.js"
+  },
+  {
+    "revision": "0b1e8647e82c18f1bbe8b4a59fb3fbb8",
+    "url": "/random-restaurant-app/static/js/2.fb4a4f0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a157136346f07162a88",
+    "url": "/random-restaurant-app/static/js/main.91ae85da.chunk.js"
+  },
+  {
+    "revision": "347cb32c81ee97e98b9d",
+    "url": "/random-restaurant-app/static/js/runtime-main.2096be17.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/random-restaurant-app/precache-manifest.e10ea2ad1db7ff923ca340fea2242846.js"
+  "/random-restaurant-app/precache-manifest.3b92ed050cecd321492b583befef6eb1.js"
 );
 
 self.addEventListener('message', (event) => {
